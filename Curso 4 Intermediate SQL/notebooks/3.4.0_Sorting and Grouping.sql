9dc2d0f14478f7d216b07d29d99e794ecc8f2901
oecorrechag <oecorrechag@unal.edu.co>
2023-06-11 10:54:40 -0500

    3.4.3

--- Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
+++ Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
@@ -13,5 +13,17 @@
 -- ORDER BY duration DESC;
 
 
+-- ## 3.4.3 Sorting multiple fields
+
+-- Select the release year, duration, and title sorted by release year and duration
+-- SELECT release_year, duration, title
+-- FROM films
+-- ORDER BY release_year DESC, duration DESC;
+
+-- Select the certification, release year, and title sorted by certification and release year
+-- SELECT certification, release_year, title 
+-- FROM films 
+-- ORDER BY certification ASC, release_year DESC;
+
 
 
