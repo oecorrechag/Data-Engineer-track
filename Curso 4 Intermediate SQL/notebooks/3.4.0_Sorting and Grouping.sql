9b52fa70bd01b574f82fa87d50abb9d6f61c7bcc
oecorrechag <oecorrechag@unal.edu.co>
2023-06-11 10:47:59 -0500

    3.4.2

--- Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
+++ Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
@@ -1,3 +1,17 @@
 -- SQLite
 
--- ## 0.4.3 Sorting single fields
+-- ## 3.4.2 Sorting single fields
+
+-- Select name from people and sort alphabetically
+-- Select name
+-- FROM people
+-- ORDER BY name;
+
+-- Select the title and duration from longest to shortest film
+-- Select title, duration
+-- FROM films
+-- ORDER BY duration DESC;
+
+
+
+
