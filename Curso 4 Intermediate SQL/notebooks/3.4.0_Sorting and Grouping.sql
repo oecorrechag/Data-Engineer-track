1a5112bf4c7b560f3e8593d935b4e233ec002210
oecorrechag <oecorrechag@unal.edu.co>
2023-06-11 11:40:13 -0500

    3.4.9

--- Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
+++ Curso 4 Intermediate SQL/notebooks/3.4.0_Sorting and Grouping.sql
@@ -57,3 +57,14 @@
 -- ORDER BY language DESC;
 
 
+-- ## 3.4.9 Filter with HAVING
+
+-- Select the country and distinct count of certification as certification_count
+SELECT country, COUNT(DISTINCT certification) AS certification_count
+FROM films
+-- Group by country
+GROUP BY country
+-- Filter results to countries with more than 10 different certifications
+HAVING COUNT(DISTINCT certification) > 10;
+
+
