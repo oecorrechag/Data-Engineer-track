ae884a370c383be3b0dc195879e3ad300db9d15a
oecorrechag <oecorrechag@unal.edu.co>
2023-06-17 18:21:17 -0500

    5.1.3

--- Curso 5 Joining Data in SQL/notebooks/5.1.0_Introducing Inner Joins.sql
+++ Curso 5 Joining Data in SQL/notebooks/5.1.0_Introducing Inner Joins.sql
@@ -16,6 +16,13 @@ INNER JOIN countries
 ON cities.country_code = countries.code;
 
 
+-- ## 5.2.3 Joining with aliased tables
 
-
+-- Select fields with aliases
+SELECT c.code AS country_code, name, year, inflation_rate
+FROM countries AS c
+-- Join to economies (alias e)
+INNER JOIN economies AS e
+-- Match on code field using table aliases
+ON e.code = c.code;
 
