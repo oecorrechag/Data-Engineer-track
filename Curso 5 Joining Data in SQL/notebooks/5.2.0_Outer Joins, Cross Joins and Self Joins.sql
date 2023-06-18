7f496b778ba0b964f93acc3073cf320e09929f76
oecorrechag <oecorrechag@unal.edu.co>
2023-06-18 08:39:58 -0500

    5.2.4

--- Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
+++ Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
@@ -27,6 +27,38 @@ ON c1.country_code = c2.code
 ORDER BY code DESC;
 
 
+-- ## 5.3.4 Building on your LEFT JOIN
+
+-- Match on code fields, Filter for the year 2010
+SELECT name, region, gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+ON c.code = e.code
+WHERE year = 2010;
+
+-- Select region, and average gdp_percapita as avg_gdp
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+-- Group by region
+GROUP BY region;
+
+-- Order by descending avg_gdp, Return only first 10 records
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+GROUP BY region
+ORDER BY avg_gdp DESC
+LIMIT 10
+
+
+-- ## 5.3.5 Is this RIGHT?
+
+
 
 
 
