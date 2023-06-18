84f41191fc078cfa7f289914883dbdec184a1581
oecorrechag <oecorrechag@unal.edu.co>
2023-06-18 11:41:14 -0500

    5.2.11

--- Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
+++ Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
@@ -134,7 +134,20 @@ WHERE c.code in ('PAK','IND')
 
 -- ## 5.3.11 Choosing your join
 
-
+SELECT 
+	c.name AS country,
+    region,
+    life_expectancy AS life_exp
+FROM countries AS c
+-- Join to populations (alias as p) using an appropriate join
+LEFT JOIN populations AS p
+ON c.code = p.country_code
+-- Filter for only results in the year 2010
+WHERE year = 2010
+-- Sort by life_exp
+ORDER BY life_exp
+-- Limit to five records
+LIMIT 5;
 
 
 
