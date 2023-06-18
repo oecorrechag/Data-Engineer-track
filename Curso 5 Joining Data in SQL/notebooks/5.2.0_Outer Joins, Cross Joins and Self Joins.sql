09c19f9c37acbb4179a3f77bd55bd5924f6b7b84
oecorrechag <oecorrechag@unal.edu.co>
2023-06-18 11:57:41 -0500

    5.2.11

--- Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
+++ Curso 5 Joining Data in SQL/notebooks/5.2.0_Outer Joins, Cross Joins and Self Joins.sql
@@ -150,4 +150,10 @@ ORDER BY life_exp
 LIMIT 5;
 
 
+-- ## 5.3.12 Comparing a country to itself
 
+-- Select aliased fields from populations as p1  -> melt
+SELECT p1.country_code, p1.size AS size2010, p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
