d0cc48d94ca976cd795254691d3d249695422f74
oecorrechag <oecorrechag@unal.edu.co>
2023-06-19 15:03:47 -0500

    5.4.10

--- Curso 5 Joining Data in SQL/notebooks/5.4.0_Subqueries.sql
+++ Curso 5 Joining Data in SQL/notebooks/5.4.0_Subqueries.sql
@@ -107,9 +107,18 @@ FROM countries,
 ORDER BY lang_num DESC;
 
 
+-- ## 5.4.10 Subquery challenge
 
-
-
+-- Select relevant fields, Subquery returning country codes filtered on gov_form
+SELECT code, inflation_rate, unemployment_rate
+FROM economies
+ WHERE year = 2015 AND code NOT IN
+	(SELECT code
+   FROM countries
+   WHERE (gov_form LIKE '%Monarchy%' 
+       OR gov_form LIKE '%Republic%')
+  )
+ORDER BY inflation_rate;
 
 
 
