daf23a30a516a90085458f6ce4240d7f6e13ba24
oecorrechag <oecorrechag@unal.edu.co>
2023-07-03 15:19:58 -0500

    6.1.8

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.1.0_Your first database.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.1.0_Your first database.sql
@@ -76,6 +76,24 @@ ALTER TABLE affiliations
 DROP COLUMN university_shortname;
 
 
+-- ## 6.1.8 Migrate data with INSERT INTO SELECT DISTINCT
 
+-- Insert unique professors into the new table
+INSERT INTO professors 
+SELECT DISTINCT firstname, lastname, university_shortname 
+FROM university_professors;
+
+-- Doublecheck the contents of professors
+SELECT * 
+FROM professors;
+
+-- Insert unique affiliations into the new table
+INSERT INTO affiliations 
+SELECT DISTINCT firstname, lastname, function, organization 
+FROM university_professors;
+
+-- Doublecheck the contents of affiliations
+SELECT * 
+FROM affiliations;
 
 
