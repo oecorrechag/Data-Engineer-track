6973331835c72d2e50868d4f0f068b2ad48715d6
oecorrechag <oecorrechag@unal.edu.co>
2023-07-03 15:10:22 -0500

    6.1.7

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.1.0_Your first database.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.1.0_Your first database.sql
@@ -65,3 +65,17 @@ SELECT *
 FROM professors
 
 
+-- ## 6.1.7 RENAME and DROP COLUMNs in affiliations
+
+-- Rename the organisation column
+ALTER TABLE affiliations
+RENAME COLUMN organisation TO organization;
+
+-- Delete the university_shortname column
+ALTER TABLE affiliations
+DROP COLUMN university_shortname;
+
+
+
+
+
