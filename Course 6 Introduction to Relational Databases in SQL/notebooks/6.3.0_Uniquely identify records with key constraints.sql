1437c6e55499a57628766353e806188b871854e6
oecorrechag <oecorrechag@unal.edu.co>
2023-07-05 20:09:46 -0500

    6.3.2

---
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.3.0_Uniquely identify records with key constraints.sql
@@ -0,0 +1,20 @@
+-- SQLite
+
+-- # 6.3.0_Uniquely identify records with key constraints
+
+-- ## 6.3.2 Get to know SELECT COUNT DISTINCT
+
+-- Count the number of rows in universities
+SELECT DISTINCT COUNT (*)
+FROM universities;
+
+-- Count the number of distinct values in the university_city column
+SELECT COUNT(DISTINCT(university_city))
+FROM universities;
+
+
+-- ## 6.3.3 Identify keys with SELECT COUNT DISTINCT
+
+
+-- Falta bd
+
