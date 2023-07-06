8ce33e914aea17a2a7ca1911b4ba8a971152aff4
oecorrechag <oecorrechag@unal.edu.co>
2023-07-06 08:09:25 -0500

    6.3.6

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.3.0_Uniquely identify records with key constraints.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.3.0_Uniquely identify records with key constraints.sql
@@ -18,3 +18,31 @@ FROM universities;
 -- Try out different combinations
 SELECT COUNT(DISTINCT(firstname, lastname)) 
 FROM professors;
+
+
+-- ## 6.3.6 ADD key CONSTRAINTs to the tables
+
+-- Rename the organization column to id
+ALTER TABLE organizations
+RENAME COLUMN organization TO id;
+
+-- Make id a primary key
+ALTER TABLE organizations
+ADD CONSTRAINT organization_pk PRIMARY KEY (id);
+
+-- Rename the university_shortname column to id
+ALTER TABLE universities
+RENAME COLUMN university_shortname TO id;
+
+-- Make id a primary key
+ALTER TABLE universities
+ADD CONSTRAINT university_pk PRIMARY KEY (id);
+
+
+
+
+
+
+
+
+
