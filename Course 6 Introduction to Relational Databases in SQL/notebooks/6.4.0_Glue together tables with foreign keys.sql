fb2b1a7bfc5225095732ebd5290938a37a127d20
oecorrechag <oecorrechag@unal.edu.co>
2023-07-08 23:40:30 -0500

    Chapter 4

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
@@ -105,3 +105,44 @@ JOIN professors
 ON affiliations.professor_id = professors.id
 GROUP BY professors.university_id
 ORDER BY count DESC;
+
+
+-- ## 6.4.14 Join all the tables together
+
+-- Join all tables
+SELECT *
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id;
+
+-- Group the table by organization sector, professor ID and university city
+SELECT COUNT(*), organizations.organization_sector,
+professors.id, universities.university_city
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id
+GROUP BY organizations.organization_sector,
+professors.id, universities.university_city;
+
+-- Filter the table and sort it
+SELECT COUNT(*), organizations.organization_sector,
+professors.id, universities.university_city
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id
+WHERE organizations.organization_sector = 'Media & communication'
+GROUP BY organizations.organization_sector,
+professors.id, universities.university_city
+ORDER BY COUNT DESC;
