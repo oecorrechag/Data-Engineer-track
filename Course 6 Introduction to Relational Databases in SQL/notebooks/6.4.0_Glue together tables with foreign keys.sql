e56ffe4ed7847c551d0635646bde1e8bd047217c
oecorrechag <oecorrechag@unal.edu.co>
2023-07-08 22:31:49 -0500

    6.4.6

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
@@ -20,13 +20,29 @@ INSERT INTO professors (firstname, lastname, university_id)
 VALUES ('Albert', 'Einstein', 'UZH');
 
 
+-- ## 6.4.4 JOIN tables linked by a foreign key
+
+-- Select all professors working for universities in the city of Zurich
+SELECT professors.lastname, universities.id, universities.university_city
+FROM professors
+JOIN universities
+ON professors.university_id = universities.id
+AND universities.university_city = 'Zurich';
 
 
+-- ## 6.4.6 Add foreign keys to the "affiliations" table
 
+-- Add a professor_id column
+ALTER TABLE affiliations
+ADD COLUMN professor_id integer REFERENCES professors (id);
 
+-- Rename the organization column to organization_id
+ALTER TABLE affiliations
+RENAME organization TO organization_id
 
-
-
+-- Add a foreign key on organization_id
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id);
 
 
 
