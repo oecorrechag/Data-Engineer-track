6d36cbde186b715aeccea300fcfef5ad5f44f247
oecorrechag <oecorrechag@unal.edu.co>
2023-07-08 22:56:40 -0500

    6.4.8

--- Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
+++ Course 6 Introduction to Relational Databases in SQL/notebooks/6.4.0_Glue together tables with foreign keys.sql
@@ -59,8 +59,21 @@ FROM professors
 WHERE affiliations.firstname = professors.firstname AND affiliations.lastname = professors.lastname;
 
 
+-- Have a look at the 10 first rows of affiliations again
+SELECT * 
+FROM affiliations 
+LIMIT 10;
 
 
+-- ## 6.4.8 Drop "firstname" and "lastname"
+
+-- Drop the firstname column
+ALTER TABLE affiliations
+DROP COLUMN firstname;
+
+-- Drop the lastname column
+ALTER TABLE affiliations
+DROP COLUMN lastname;
 
 
 
