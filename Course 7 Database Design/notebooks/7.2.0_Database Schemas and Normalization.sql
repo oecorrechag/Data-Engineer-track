af44774daa1780a89c92a260bf388cc9a02f3d51
oecorrechag <oecorrechag@unal.edu.co>
2023-07-16 08:15:32 -0500

    7.2.4

--- Course 7 Database Design/notebooks/7.2.0_Database Schemas and Normalization.sql
+++ Course 7 Database Design/notebooks/7.2.0_Database Schemas and Normalization.sql
@@ -15,3 +15,22 @@ FOREIGN KEY (time_id) REFERENCES dim_time_star (time_id);
 ALTER TABLE fact_booksales ADD CONSTRAINT sales_store
 FOREIGN KEY (store_id) REFERENCES dim_store_star (store_id);
 
+
+-- ## 7.2.4 Extending the book dimension
+
+-- Create dim_author with an author column
+CREATE TABLE dim_author (
+    author varchar(256)  NOT NULL
+);
+
+-- Insert authors into the new table
+INSERT INTO dim_author
+SELECT DISTINCT author FROM dim_book_star;   
+
+-- Add a primary key 
+-- ALTER TABLE dim_author
+-- ADD COLUMN author_id SERIAL PRIMARY KEY; 
+
+-- Output the new table
+SELECT * 
+FROM dim_author;
