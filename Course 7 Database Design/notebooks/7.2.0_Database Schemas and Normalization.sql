28d4c02d3f9e4f2d959292159efb44a4920af2e1
oecorrechag <oecorrechag@unal.edu.co>
2023-07-19 07:23:36 -0500

    7.2.11

--- Course 7 Database Design/notebooks/7.2.0_Database Schemas and Normalization.sql
+++ Course 7 Database Design/notebooks/7.2.0_Database Schemas and Normalization.sql
@@ -107,6 +107,26 @@ CREATE TABLE cust_rentals (
 -- DROP COLUMN invoice_id
 
 
+-- ## 7.2.11 Converting to 2NF
 
+-- Create a new table to satisfy 2NF
+DROP TABLE IF EXISTS cars;
+            
+CREATE TABLE cars (
+    car_id VARCHAR(256) NULL,
+    model VARCHAR(128),
+    manufacturer VARCHAR(128),
+    type_car VARCHAR(128),
+    condition VARCHAR(128),
+    color VARCHAR(128)
+);
+
+-- Drop columns in customer_rentals to satisfy 2NF
+ALTER TABLE customer_rentals
+DROP COLUMN model,
+DROP COLUMN manufacturer, 
+DROP COLUMN type_car,
+DROP COLUMN condition,
+DROP COLUMN color
 
 
