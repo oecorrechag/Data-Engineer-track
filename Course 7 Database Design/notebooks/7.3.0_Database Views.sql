897fef8a094052a70689569bb417e79331833901
oecorrechag <oecorrechag@unal.edu.co>
2023-07-21 08:03:10 -0500

    7.3.4

--- Course 7 Database Design/notebooks/7.3.0_Database Views.sql
+++ Course 7 Database Design/notebooks/7.3.0_Database Views.sql
@@ -156,6 +156,32 @@ DROP COLUMN type_car;
 -- WHERE table_schema NOT IN ('pg_catalog', 'information_schema');
 
 
+-- ## 7.3.4 Creating and querying a view
+
+-- Create a view for reviews with a score above 9
+CREATE VIEW high_scores AS
+SELECT * 
+FROM reviews
+WHERE score > 9;
+
+-- Count the number of self-released works in high_scores
+SELECT COUNT(*) 
+FROM high_scores
+INNER JOIN label ON high_scores.reviewid = labels.reviewid
+WHERE label = 'self-released';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
