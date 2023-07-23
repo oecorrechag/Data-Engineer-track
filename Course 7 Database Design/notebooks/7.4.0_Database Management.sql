0f62f05cc9d332f2a8e386ec2ffb6fbd7dcce2b4
oecorrechag <oecorrechag@unal.edu.co>
2023-07-23 11:37:47 -0500

    7.4.3

--- Course 7 Database Design/notebooks/7.4.0_Database Management.sql
+++ Course 7 Database Design/notebooks/7.4.0_Database Management.sql
@@ -14,5 +14,12 @@ Create a data scientist role
 CREATE ROLE admin WITH CREATEDB CREATEROLE;
 
 
+-- ## 7.4.3 GRANT privileges and ALTER attributes
+
+-- Grant data_scientist update and insert privileges
+GRANT UPDATE, INSERT ON long_reviews TO data_scientist;
+
+-- Give Marta's role a password
+ALTER ROLE marta WITH PASSWORD 's3cur3p@ssw0rd';
 
 
