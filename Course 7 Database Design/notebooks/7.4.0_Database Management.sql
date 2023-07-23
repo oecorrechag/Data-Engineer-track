327c436d9c0fd9370bac88290ad57c3563da8054
oecorrechag <oecorrechag@unal.edu.co>
2023-07-23 00:18:57 -0500

    7.4.2

--- Course 7 Database Design/notebooks/7.4.0_Database Management.sql
+++ Course 7 Database Design/notebooks/7.4.0_Database Management.sql
@@ -4,5 +4,15 @@
 
 -- ## 7.4.2 Create a role
 
+-- Create a data scientist role
+CREATE ROLE data_scientist;
+
+-- Create a role for Marta
+Create a data scientist role
+
+-- Create an admin role
+CREATE ROLE admin WITH CREATEDB CREATEROLE;
+
+
 
 
